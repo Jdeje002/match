a8e059cf2456f6da4bd41e50b0fff189ce86d963
John De Jesus <Jd@dhcp-v074-142.mobile.uci.edu>
2018-06-04 17:04:34 -0700

    added img and bootstraps

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import Instruction from './Components/Instruction';
 import card from './cards.json'
 import Cards from "./Components/Cards"
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App ">
        <nav>
         <NavBar/>
        </nav>
@@ -18,12 +19,15 @@ class App extends Component {
        <Instruction/>
        </div>
       <div >
-        
-      {card.map(card=> <Cards name= {card.name} img = {card.image} />)
+        <div >
+        <div  ></div>
+      
+      <div > {card.map(card=> <Cards name= {card.name} img = {card.image} />)
       }
-  
       </div>
-       
+      <div> </div>
+      </div>
+      </div>
       </div>
     );
   }
--- src/Components/Score/Score.js
+++ src/Components/Score/Score.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import './Score.css';
-import char from "./cards.json";
+
 
 class Score extends React.Component {
     state = {
       count: 0
     };
-  
-    
-
-       Point = () => {
-           if(char.id === char.id){
-            this.setState({count:0})
-           } else{
-            this.setState({count:this.state.count + 1 })
-           }};
   
     render() {
       return (
